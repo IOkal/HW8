97c8a7ac70bf7e386f2042408138d093505e9951
mmanka1 <33776177+mmanka1@users.noreply.github.com>
2021-11-19 22:40:41 -0500

    Initial setup

---
+++ src/components/menuItems.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+
+
+const MenuItems = () => {
+    const [retreivedItems, setRetrievedItems] = UseState([]);
+    const [sendMenuItem, setSendMenuItem] = UseState({});
+
+    useEffect(() => {
+
+    })
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
