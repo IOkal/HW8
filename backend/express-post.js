521b4059b9cb2d4839019bd8e895c0427ea61dc3
Mohammad Hasan <mohd.hasan@mail.utoronto.ca>
2021-11-20 18:04:05 -0500

    Update express-post.js

--- backend/express-post.js
+++ backend/express-post.js
@@ -11,10 +11,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // Route that receives a POST request to /sms
 app.post('/sms', function (req, res) {
-  const phonenumber = req.query.phoneNumber;
-  const fileURL = req.query.fileURL;
+  const phonenumber = req.body.From;
+  // const fileURL = req.query.fileURL;
+  const body = req.body.Body;
+  // const body = JSON.stringify(req.body);
   res.set('Content-Type', 'text/plain');
-  res.send(`You sent: ${fileURL} to Express`);
+  res.send(`You sent: ${body}, ${phonenumber} to Express`);
 });
 
 // Tell our app to listen on port 3000
