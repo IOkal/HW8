791ebbfe87dcae9b88063861d4ae177fe8127619
iokal <iokal@uwo.ca>
2021-11-21 00:14:33 -0500

    Running through code to figure out changes

--- backend/parsingWords.js
+++ backend/parsingWords.js
@@ -11,11 +11,10 @@
 //     return mappings[word.lower()];
 // }
 
-
 function checkIfWordInList (word, lst) {
 
     for (var i = 0; i < lst.length; i++) {
-        if (lst[i] === word) {
+        if (lst[i].toLowerCase() === word.toLowerCase()) {
             return true;
         }
     }
@@ -23,72 +22,98 @@ function checkIfWordInList (word, lst) {
     return false;
 }
 
-let json = {
-    "paragraphs": []
-};
+function detectWords() {
+    let json = {
+        "paragraphs": []
+    };
 
-// test file
-const body = JSON.parse("results_output-1-to-4.json");
+    var mappings = {
+    "pork": ["pig", "bacon", "prosciutto", "ribs", "pancetta", "gammon", "rasher", "sowbelly", "pork slab", "porkbelly", "hog"],
+    "alcohol": ["wine", "beer", "vodka", "booze", "liquor", "rose"],
+    "mushroom": ["funghi"],
+    "gluten": ["wheat", "rye", "couscous", "bran", "bread", "pita", "bagel", "flatbread", "roll", "pasta", "cake", "crackers",
+    "biscuits", "pie", "pastry", "crouton", "malt", "yeast", "fries", "gravy", "soy", "chips"],
+        "vegan": ["meat", "pork", "beef", "steak", "chicken", "fish", "shrimp", "seafood", "bacon", "egg", "cheese", "milk"]
+    };
 
-for (var i = 0; i < body["responses"].length; i++) {
+    var redList = ["pizza"];
+    var yellowList = ["pasta"];
+    var greenList = ["chicken"];
 
-    var response = body["responses"][i];
-    var pages = response["fullTextAnnotation"]["pages"];
+    var fs = require("fs");  
+    var body2 = fs.readFileSync("../results_output-1-to-4.json", "UTF-8");  
+    var body = JSON.parse(body2);
+    // console.log(body)
+    // console.log(data);  
 
-    var blocks = pages[0]["blocks"];
+    // test file
+    // const body = JSON.parse("../results_output-1-to-4.json");
+    // console.log(body.responses[1].fullTextAnnotation.pages[0].blocks[0].paragraphs[0].words[0].symbols[0].text)
 
+    for (var i = 0; i < body["responses"].length; i++) {
 
-    for (var j = 0; j < blocks.length; j++) {
+        var response = body["responses"][i];
+        var pages = response["fullTextAnnotation"]["pages"];
 
-        var paragraphs = blocks["paragraphs"];
+        var blocks = pages[0]["blocks"];
 
-        for (var k = 0; k < paragraphs.length; k++) {
+        for (var j = 0; j < blocks.length; j++) {
 
-            var paragraph = {};
+            var paragraphs = blocks[j]["paragraphs"];
 
-            var words = paragraphs[k]["words"];
-            var numYellow = 0;
-            var numGreen = 0;
+            for (var k = 0; k < paragraphs.length; k++) {
 
-            bool b = false;
-            for (var w = 0; w < words.length; w++) {
-                var word = "";
-                var symbols = words[w];
+                var paragraph = [];
 
-                for (let s = 0; s < symbols.length; s++) {
-                    word += symbols[s];
+                var words = paragraphs[k]["words"];
+                var numYellow = 0;
+                var numGreen = 0;
+
+                var b = false;
+                for (var w = 0; w < words.length; w++) {
+                    var word = "";
+                    var symbols = words[w];
+                    // console.log(symbols)
+
+                    // console.log(symbols.length)
+                    for (let s = 0; s < symbols.length; s++) {
+                        word = word + symbols[s].text;
+                    }
+                    // console.log(word)
+
+                    if (checkIfWordInList(word, redList)) {
+                        paragraph.push({"boundingBox": paragraphs[k].boundingBox, "Status": "R"});
+                        b = true;
+                        break;
+                    }
+                    else if (checkIfWordInList(word, yellowList)) {
+                        numYellow++;
+                    }
+                    else if (checkIfWordInList(word, greenList)) {
+                        numGreen++;
+                    }
+
+                }
+                if (b) break;
+
+                if (numYellow > 0) {
+                    paragraph.push({"boundingBox": paragraphs[k].boundingBox, "Status": "Y"});
+                } else if (numGreen > 0) {
+                    paragraph.push({"boundingBox": paragraphs[k].boundingBox, "Status": "G"});
+                } else {
+                    paragraph.push({"boundingBox": paragraphs[k].boundingBox, "Status": "N"});
                 }
 
-                if (checkIfWordInList(word, redList)) {
-                    paragraph.push({"boundingBox": paragraphs[k].boundingBox, "Status": "R"});
-                    b = true;
-                    break;
-                }
-                else if (checkIfWordInList(word, yellowList)) {
-                    numYellow++;
-                }
-                else if (checkIfWordInList(word, greenList)) {
-                    numGreen++;
-                }
-
+                json["paragraphs"].push(paragraph);
+                fs.writeFile ("input.json", JSON.stringify(json), function(err) {
+                    if (err) throw err;
+                    // console.log('complete');
+                    }
+                );
+                // console.log(JSON.stringify(json));
             }
-            if (b) break;
-
-            if (numYellow > 0) {
-                paragraph.push({"boundingBox": paragraphs[k].boundingBox, "Status": "Y"});
-            } else if (numGreen > 0) {
-                paragraph.push({"boundingBox": paragraphs[k].boundingBox, "Status": "G"});
-            } else {
-                paragraph.push({"boundingBox": paragraphs[k].boundingBox, "Status": "N"});
-            }
-
-            json["paragraphs"].push(paragraph);
-
         }
-
     }
-
-
-
-
 }
+
+detectWords();
