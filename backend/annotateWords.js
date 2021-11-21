8e1bd9dfd5e4b2b7e02776ca5598ddd05da65aae
Mohammad Hasan <mohd.hasan@mail.utoronto.ca>
2021-11-21 03:09:55 -0500

    Update annotateWords.js

--- backend/annotateWords.js
+++ backend/annotateWords.js
@@ -6,7 +6,6 @@ async function wordsAnnotation(filename) {
     await PDFNet.initialize('demo:1637473300094:7896b7a90300000000ed9ab4130b1edd11f2cffde0d63817bdc7f78845');
     const doc = await PDFNet.PDFDoc.createFromFilePath('./LCL-FoodMenu-June2021.pdf');
     // const doc = await PDFNet.PDFDoc.createFromURL('12896892489-2021-11-20.pdf');
-    const page = await doc.getPage(1);
 
     var body2 = fs.readFileSync("./input.json", "UTF-8");
     var body = JSON.parse(body2);
@@ -26,6 +25,7 @@ async function wordsAnnotation(filename) {
             var paragraph = paragraphs[i][0];
 
             var status = paragraph["Status"];
+            var pageNum = paragraph["Page"];
             var normalizedBounds = paragraph["boundingBox"]["normalizedVertices"];
 
             var lowerLeftX = normalizedBounds[0]["x"] * width;
@@ -46,6 +46,7 @@ async function wordsAnnotation(filename) {
                 hl.setColor((await PDFNet.ColorPt.init(1, 1, 0.2)), 3);
             }
 
+            const page = await doc.getPage(pageNum);
             hl.refreshAppearance();
             page.annotPushBack(hl);
 
